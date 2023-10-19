a4de445c17f85dce389e2dc8f8ea5846fc616b1c
saurav <sauravlokhande24@gmail.com>
2023-10-20 01:16:24 +0530

    last last final commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express') ;
 const app = express() ;
 const mongoose = require('mongoose') ;
-const PORT = 5000 ;
+const PORT = process.env.PORT || 5000;
 const cors = require('cors');
 app.use(cors());
 //  const{MONGODB_URI} = require('./config') ;
